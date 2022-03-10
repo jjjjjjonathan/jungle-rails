e32ed5861f7ea84d81a247f0e85354fbfa2908a4
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-03-10 14:02:32 -0500

    Generate admin category view and partial

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,6 +2,15 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PASSWORD"]
 
   def index
+    @categories = Category.order(id: :desc).all
   end
-  
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
 end
