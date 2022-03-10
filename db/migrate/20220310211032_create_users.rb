722708dcaf111d9da444fdd8956f4f1f4e42f4ad
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-03-10 16:16:13 -0500

    Fix registration

--- db/migrate/20220310211032_create_users.rb
+++ db/migrate/20220310211032_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :given_name
+      t.string :surname
       t.string :email
       t.string :password_digest
 
