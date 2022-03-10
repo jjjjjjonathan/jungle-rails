0b8da4818b943f3ac8e8d6c6935c3ea8b7d3346e
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-03-10 17:45:08 -0500

    Fix login functionality and add buttons to nav

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.find_by_email(params[:user][:email])
+    puts "hello #{user}"
+    if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
+      puts "NOPE SOMETHING WRONG"
       redirect_to '/login'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -19,12 +19,14 @@ Rails.application.routes.draw do
     resources :categories, :products, except: [:edit, :update, :show]
   end
 
-  get '/register', to: 'users#new'
-  post '/users', to: "users#create"
-
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+
+  get '/register', to: 'users#new'
+  post '/users', to: "users#create"
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
