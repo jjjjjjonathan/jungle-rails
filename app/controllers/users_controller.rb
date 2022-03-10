228cd36370383b05aef57c6fd8a7784fac29f0bb
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-03-10 16:02:52 -0500

    Create login and register forms

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/reigster'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :given_name
+      :surname
+      :email
+      :password
+      :password_confirmation
+    )
+  end
+end
